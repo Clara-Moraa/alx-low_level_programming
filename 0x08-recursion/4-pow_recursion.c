37df667e979b05bc7cd4215024b42303aabe3353
Clara-Moraa <moraacalaine@gmail.com>
2023-04-05 02:03:55 -0700

    calculate power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,26 +1,17 @@
 #include "main.h"
-
 /**
- * calculatePower - calculates the power of a number recursively.
- *
- * @x: The base number.
- * @y: The exponent number.
- *
- * Return: The result of x raised to the power of y, or -1 if y is less than 0.
+ * _pow_recursion - prints the value of the  power of a number.
+ * @x: number to be raised by a power(base).
+ * @y: power of a number to raise(exponent).
+ * Return: -1 if y is lower than 0;
+ * else return value.
  */
-int calculatePower(int x, int y)
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	{
 		return (-1);
-	}
 	else if (y == 0)
-	{
 		return (1);
-	}
 	else
-	{
-		return (x * calculatePower(x, y - 1));
-	}
+		return (x * _pow_recursion(x, y - 1));
 }
-
