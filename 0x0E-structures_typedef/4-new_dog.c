08379bfb0989837c355e61a7f5f1ce407642b492
Clara-Moraa <moraacalaine@gmail.com>
2023-04-17 03:23:07 -0700

    new owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,6 +9,31 @@
  *
  * Return: pointer to new dog
  */
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
+char *_strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+
+	return (dest);
+}
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *tessie;
@@ -35,7 +60,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	tessie->name = _strcpy(tessie->name, name);
+	tessie->name = _strcopy(tessie->name, name);
 	tessie->age = age;
 	tessie->owner = _strcopy(tessie->owner, owner);
 
